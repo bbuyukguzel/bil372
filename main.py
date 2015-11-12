50c8c284fc9bf4a19760c6dc8f910da25194290a
Batuhan Buyukguzel <bbuyukguzel@gmail.com>
2015-11-12 20:10:15 +0200

    Add getting source code function

---
+++ main.py
@@ -0,0 +1,13 @@
+import urllib
+import requests
+
+
+def get_source_code(url):
+        try:
+            user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
+            headers = {'User-Agent': user_agent, }
+            request = urllib.request.Request(url, None, headers)
+            response = urllib.request.urlopen(request)
+            return response.read()
+        except requests.exceptions.RequestException as e:
+            print(e)
