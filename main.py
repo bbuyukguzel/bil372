154c2cf1629d794399bdb016bacf8dd227672511
AyberkHalac <AyberkHalac@users.noreply.github.com>
2015-11-13 05:05:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,26 @@ def get_source_code(url):
         headers = {'User-Agent': user_agent, }
         request = urllib.request.Request(url, None, headers)
         response = urllib.request.urlopen(request)
-        return response.read()
+        code = response.read()
+
+        # don't care between style tags
+        while (code.find(b'<style>') != -1):
+            s_index = code.find(b'<style>')
+            e_index = code.find(b'</style>', s_index)
+            code = code[:s_index] + code[e_index + (len('</style>')):]
+        # don't care between script tags
+        while (code.find(b'<script') != -1):
+            s_index = code.find(b'<script')
+            e_index = code.find(b'</script>', s_index)
+            code = code[:s_index] + code[e_index + (len('</script>')):]
+        return code
     except requests.exceptions.RequestException as e:
         print(e)
+
+
+def find_phone(code):
+    pattern2 = b'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})'
+    print(re.findall(pattern2, code))
 
 
 def url_list():
