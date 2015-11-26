2762ceb23206bd8fdb5dd3b73a3d7b1c0acd8ef6
Batuhan Buyukguzel <bbuyukguzel@gmail.com>
2015-11-26 02:31:45 +0200

    Improve regex for  find_email

--- info_parser.py
+++ info_parser.py
@@ -45,7 +45,11 @@ class Parser:
 
     def find_email(self):
         pattern = r'(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])'
+        pattern2 = r'(^[a-zA-Z0-9_.+-]+(@|(\s?(\{|\(|\[)\s?(at|AT)\s?(\}|\)|\])\s?)|(\s(at|AT|@)\s))[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)'
         # url fonksiyonu eklendiginde contact ve turevi sayfalarda da arama yap
         # mailto ile başlayabilir
-        # print(re.findall(pattern, self.source))
-        return re.findall(pattern, self.source)
+
+        if len(re.findall(pattern, self.source)) > 0:
+            return re.findall(pattern, self.source)
+        else:
+            return re.findall(pattern2, self.source)
