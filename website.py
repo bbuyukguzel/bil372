1f84dda63862cb6113afcac847d76fca3005e483
Yasar Budulgan <yb.budulgan@gmail.com>
2015-12-04 03:11:14 +0200

    * Added filter for publication

--- website.py
+++ website.py
@@ -85,6 +85,12 @@ def result():
             res = searchInInterest(keyword)
             for i in res:
                 l.append(i[0])
+
+        if("yayinlar" in filter):
+            res = searchInPub(keyword)
+            for i in res:
+                l.append(i[0])
+
     else:
         res = searchInBio(keyword)
         for i in res:
@@ -106,6 +112,11 @@ def result():
             if(not i[0] in l):
                 l.append(i[0])
 
+        res = searchInPub(keyword)
+        for i in res:
+            if(not i[0] in l):
+                l.append(i[0])
+
     namelist = list()
     for i in l:
         namelist.append(searchByID(i))
@@ -117,15 +128,15 @@ def searchByID(id):
     c = db.connect()
     liste = list()
 
-    query = "select fname, lname from bio where pid=\'"+str(id)+"\'"
+    query = "select bio.pid, fname, lname, title, university from bio, work " \
+                    "where bio.pid = work.pid and bio.pid=\'"+str(id)+"\'"
     res = c.execute(query)
 
     if(res):
         for i in res:
              return i
 
-
-    return list("None", "None")
+    return ("None", "None", "None", "None")
 
 
 def db_connect():
@@ -173,6 +184,24 @@ def searchInInterest(keyword):
 
     return [i for i in f if f]
 
+def searchInPub(keyword):
+    c = db.connect()
+    query = "select pubid from publication where pubname"+" ilike \'"+keyword+"\'"
+    res = c.execute(query)
+    f = res.fetchall()
+
+    pubid_list = [i[0] for i in f if f]
+    id_list = list()
+
+    for i in pubid_list:
+        query = "select pid from published where pubid=\'"+str(i)+"\';"
+        res = c.execute(query)
+        f = res.fetchall()
+        print(f)
+        for i in f:
+            id_list.append(i)
+
+    return id_list
 
 
 if __name__ == '__main__':
@@ -182,29 +211,3 @@ if __name__ == '__main__':
 
 
 
-"""
-def select(table, cols, const):
-    liste = list()
-    c = db.connect()
-    query = "select "
-
-    if(type(cols) == str):
-        query += cols + " from " + table
-    elif(type(cols) == list):
-        for i in cols:
-            query += i + " "
-        query += "from " + table
-
-
-    if(type(const) == dict):
-        query += " where "
-        for i in const:
-            query += i + "=" + const[i]
-
-    a = c.execute(query)
-
-    for i in a:
-        liste.append(i)
-    c.close()
-    return liste
-"""
